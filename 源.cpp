3d0b510e3951d90a79b91ca9f533d8145e78eca3
LeYang <LeYang@yangle1108.com>
2023-07-15 11:22:19 +0800

    添加项目文件。

---
+++ 源.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+	for (int i = 0; i < 10; i++)
+	{
+
+	   for (int i = 0; i < 10; i++)
+	   {
+		   cout << "nihao" << endl;
+		   cout << "你好" << endl;
+		   cout << "鸟尼玛" << endl;
+
+	   }
+		
+	}
+	return 0;
+
+}
